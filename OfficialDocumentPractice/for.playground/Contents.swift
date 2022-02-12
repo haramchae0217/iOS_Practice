0918357b7cfc3376d66b2ec310d9cd03b279d5a1
Chae Haram <djvemfla2@gmail.com>
2022-02-13 01:10:04 +0900

    for문 연습

--- OfficialDocumentPractice/for.playground/Contents.swift
+++ OfficialDocumentPractice/for.playground/Contents.swift
@@ -6,4 +6,34 @@ for name in names {
 }
 
 let numberOfLegs = ["spider":8,"ant":6, "cat":4]
-for (animal)
+for (animalName, legCount) in numberOfLegs {
+    print("\(animalName)s have \(legCount) legs")
+}
+
+for index in 1...5 {
+    print("\(index) times 5 is \(index * 5)")
+}
+
+let base = 3
+let power = 10
+var answer = 1
+for _ in 1...power {
+    answer *= base
+}
+print("\(base) to the power of \(power) is \(answer)")
+
+let minutes = 60
+for tickMark in 0..<minutes {
+    // render the tick mark each minute (60 times)
+}
+
+let minuteInterval = 5
+for tickMark in stride(from: 0, to: minutes, by: minuteInterval) {
+    // render the tick mark every 5 minutes (0,5,10,15 ... 45,50,55)
+}
+
+let hours = 12
+let hourInterval = 3
+for tickMark in stride(from: 3, to: hours, by: hourInterval) {
+    // render the tick mark every 3 hours (3,6,9,12)
+}

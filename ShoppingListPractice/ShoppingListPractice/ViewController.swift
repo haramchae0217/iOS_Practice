b74e3b2a2d0cb12e913eda87351c3b8b5c0dc87c
Chae Haram <djvemfla2@gmail.com>
2022-03-02 18:12:55 +0900

    list추가 및 버튼체크

--- ShoppingListPractice/ShoppingListPractice/ViewController.swift
+++ ShoppingListPractice/ShoppingListPractice/ViewController.swift
@@ -20,20 +20,45 @@ class ViewController: UIViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
+    
     @IBAction func addShoppingListButton(_ sender: UIButton) {
         let shopping = shoppingListTextField.text!
         if shopping.isEmpty {
-            
+            UIAlertController.showAlert(message: "리스트를 입력하세요.", vc: self)
+            return
         }
-        
+        Shopping.ShoppingList.append(Shopping(title: shoppingListTextField.text!))
+        self.tableView.reloadData()
     }
     
+    @objc func checkButtonClicked(_ sender: UIButton) {
+        // if selcted -> 빈 체크박스 그림
+        // if not selected -> 색칠된 체크박스 그림
+        if sender.isSelected {
+            sender.setImage(UIImage(systemName: "checkmark.square"), for: .normal)
+            sender.isSelected = false
+            Shopping.ShoppingList[sender.tag].isChecked = false
+        } else {
+            sender.setImage(UIImage(systemName: "checkmark.square.fill"), for: .normal)
+            sender.isSelected = true
+            Shopping.ShoppingList[sender.tag].isChecked = true
+        }
+        print(Shopping.ShoppingList[sender.tag].isChecked)
+    }
 
 }
 
 extension ViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "shoppingListCell", for: indexPath) as? ShoppingListTableViewCell else { return UITableViewCell() }
+        
+        cell.checkButton.addTarget(self, action: #selector(checkButtonClicked), for: .touchUpInside)
+        cell.shoppingListLabel.text = Shopping.ShoppingList[indexPath.row].title
+        cell.checkButton.tag = indexPath.row
+        
         return cell
     }
     

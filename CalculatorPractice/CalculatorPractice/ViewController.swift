567e3278d893877c55c28050e82bf301ec107879
Chae Haram <djvemfla2@gmail.com>
2022-03-09 14:54:40 +0900

    계산기 기본 세팅

--- CalculatorPractice/CalculatorPractice/ViewController.swift
+++ CalculatorPractice/CalculatorPractice/ViewController.swift
@@ -9,10 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var calculateFormulaLabel: UILabel!
+    @IBOutlet weak var calculationResultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    @IBAction func numberZero(_ sender: UIButton) {
+    }
+    @IBAction func numberOne(_ sender: UIButton) {
+    }
+    @IBAction func numberTwo(_ sender: UIButton) {
+    }
+    @IBAction func numberThree(_ sender: UIButton) {
+    }
+    @IBAction func numberFour(_ sender: UIButton) {
+    }
+    @IBAction func numberFive(_ sender: UIButton) {
+    }
+    @IBAction func numberSix(_ sender: UIButton) {
+    }
+    @IBAction func numberSeven(_ sender: UIButton) {
+    }
+    @IBAction func numberEight(_ sender: UIButton) {
+    }
+    @IBAction func numberNine(_ sender: UIButton) {
+    }
+    @IBAction func plusButton(_ sender: UIButton) {
+    }
+    @IBAction func minusButton(_ sender: UIButton) {
+    }
+    @IBAction func multipleButton(_ sender: UIButton) {
+    }
+    @IBAction func devideButton(_ sender: UIButton) {
+    }
+    @IBAction func equalButton(_ sender: UIButton) {
+    }
+    @IBAction func allClearButton(_ sender: UIButton) {
+    }
+    
 
 
 }

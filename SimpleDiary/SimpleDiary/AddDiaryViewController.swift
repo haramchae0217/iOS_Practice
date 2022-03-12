7271aaaa0a76fc097bca0b62efcb2e2d0f291034
Chae Haram <djvemfla2@gmail.com>
2022-03-12 12:46:52 +0900

    textview 설정법

--- SimpleDiary/SimpleDiary/AddDiaryViewController.swift
+++ SimpleDiary/SimpleDiary/AddDiaryViewController.swift
@@ -14,11 +14,21 @@ class AddDiaryViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        if addDiaryContentTextView.text.isEmpty {
+            addDiaryContentTextView.text = "내용을 입력해주세요."
+            addDiaryContentTextView.textColor = .lightGray
+        }
+        addDiaryContentTextView.delegate = self
+        
+        
         title = "Add Diary"
         let rightBarButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(createBarButton))
           self.navigationItem.rightBarButtonItem = rightBarButton
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
         @objc func createBarButton() {
             //textView에 있는 글씨는 이전화면으로 titleLabel에 대입
             Diary.diaryData.content = addDiaryContentTextView.text
@@ -30,7 +40,19 @@ class AddDiaryViewController: UIViewController {
         
     }
     
-    
-    
+}
 
+extension AddDiaryViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.text == "내용을 입력해주세요." {
+            textView.text = ""
+            textView.textColor = .label
+        }
+    }
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = "내용을 입력해주세요."
+            textView.textColor = .lightGray
+        }
+    }
 }

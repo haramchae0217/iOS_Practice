8a47fdf119fcc358b8858939c541810a6b2ef4b7
Chae Haram <djvemfla2@gmail.com>
2022-03-19 12:03:59 +0900

    searchbar 생성

--- practiceSearchBar/practiceSearchBar/SearchViewController.swift
+++ practiceSearchBar/practiceSearchBar/SearchViewController.swift
@@ -10,24 +10,40 @@ import UIKit
 class SearchViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
+    var alienList: [String] = ["breqrains", "ghivval", "krel", "throngraks", "hummur", "elgan", "one", "dalmi", "comad", "bognaer", "equd", "trexeid", "thraerex", "vuuczils", "henairs"]
+    var filteredList: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "이름 검색"
+        
         tableView.dataSource = self
         tableView.delegate = self
         
+    // searchController
+        let searchController = UISearchController()
+        searchController.delegate = self
+        tableView.tableHeaderView = searchController.searchBar
     }
-
 
 }
 
 extension SearchViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        if filteredList.isEmpty {
+            return alienList.count
+        } else {
+            return filteredList.count
+        }
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.identifier, for: indexPath) as? SearchTableViewCell else { return UITableViewCell() }
+        if filteredList.isEmpty{
+            cell.searchLabel.text = alienList[indexPath.row]
+        } else {
+            cell.searchLabel.text = filteredList[indexPath.row]
+        }
         
         return cell
     }
@@ -35,6 +51,10 @@ extension SearchViewController: UITableViewDataSource {
 
 extension SearchViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        <#code#>
+        return 60
     }
+}
+
+extension SearchViewController: UISearchControllerDelegate {
+    
 }

7a8084e6bf8eca5e80142db3a7313ff95cdd980b
Chae Haram <djvemfla2@gmail.com>
2022-03-19 12:34:09 +0900

    검색화면 넘어가기 구현

---
+++ practiceSearchBar/practiceSearchBar/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MainViewController.swift
+//  practiceSearchBar
+//
+//  Created by Chae_Haram on 2022/03/19.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    @IBAction func searchBarButtonClicked(_ sender: UIBarButtonItem) {
+        guard let searchVC = storyboard?.instantiateViewController(withIdentifier: "searchVC") as? SearchViewController else { return }
+        self.navigationController?.pushViewController(searchVC, animated: true)
+    }
+    
+    
+
+}

8e28d68e203c52b3b4105388a98a8fe7f5dc0568
Chae Haram <djvemfla2@gmail.com>
2022-03-19 11:48:54 +0900

    tableView setting

--- practiceSearchBar/practiceSearchBar/SearchViewController.swift
+++ practiceSearchBar/practiceSearchBar/SearchViewController.swift
@@ -13,9 +13,28 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
         
     }
 
 
 }
 
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.identifier, for: indexPath) as? SearchTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        <#code#>
+    }
+}

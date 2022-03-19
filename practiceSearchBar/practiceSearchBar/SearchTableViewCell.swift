22b6b9edeed1c4360dc343f08a493881857bedea
Chae Haram <djvemfla2@gmail.com>
2022-03-19 11:46:04 +0900

    searchbar 기본 세팅

---
+++ practiceSearchBar/practiceSearchBar/SearchTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  SearchTableViewCell.swift
+//  practiceSearchBar
+//
+//  Created by Chae_Haram on 2022/03/19.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+    static let identifier = "searchCell"
+    
+    @IBOutlet weak var searchLabel: UILabel!
+    
+
+
+}
--- practiceSearchBar/practiceSearchBar/SearchViewController.swift
+++ practiceSearchBar/practiceSearchBar/SearchViewController.swift
@@ -7,8 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         

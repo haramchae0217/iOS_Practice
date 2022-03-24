7ee14977b4ce7b03f0ade1b8344c61fdb965294d
Chae Haram <djvemfla2@gmail.com>
2022-03-24 23:18:34 +0900

    slang 검색 구현

--- SlangQuiz/SlangQuiz/Source/Controllers/ViewController.swift
+++ SlangQuiz/SlangQuiz/Source/Controllers/ViewController.swift
@@ -6,6 +6,12 @@
 //
 
 import UIKit
+/*
+ 1. searchController 연결하기
+ 2. 검색어와 완전히 일치하는 항목 거르기
+    - 완전히 일치하는 단어가 없을 시에는 "혹시 찾으시는 단어가 이중에 있나요?" 라는 문구와 함께 해당 검색어가 포함된 slang 목록(short name)을 띄워준다.
+ 3. 검색어와 일치하는 항목을 shortNameLabel, fullNameLabel에 출력하기
+ */
 
 class ViewController: UIViewController {
 
@@ -13,9 +19,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var shortNameLabel: UILabel!
     @IBOutlet weak var fullNameLabel: UILabel!
     
+    var matchWord: Slang?
+    
     // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "신조어 검색기"
+        
+        let searchNameController = UISearchController(searchResultsController: nil)
+        searchNameController.searchBar.delegate = self
+        searchNameController.searchResultsUpdater = self
+        searchNameController.searchBar.placeholder = "신조어를 입력해보세요!"
+        navigationItem.searchController = searchNameController
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //shortNameLabel.text = MyDB.slangList.
         
     }
     
@@ -24,3 +44,41 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController: UISearchBarDelegate, UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        // 검색어에 따른 실시간 검색 결과 도출
+        // 일치하는 결과가 있으면 shortName & fullName 띄워주기
+        if let searchText = searchController.searchBar.text {
+            matchWord = MyDB.slangList.filter{ $0.shortName == searchText }.first
+//            if let word = matchWord {
+//                shortNameLabel.text = word.shortName
+//                fullNameLabel.text = word.fullName
+//
+//                matchWord = nil
+//            } else {
+//                shortNameLabel.text = "일치하는"
+//                fullNameLabel.text = "결과가 없어요"
+//            }
+        }
+    }
+    
+    // 취소버튼 눌렀을 때
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        shortNameLabel.text = "신조어를"
+        fullNameLabel.text = "검색해보세요"
+    }
+    // 검색버튼 눌렀을 때
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let word = matchWord {
+            shortNameLabel.text = word.shortName
+            fullNameLabel.text = word.fullName
+            
+            matchWord = nil
+        } else {
+            shortNameLabel.text = "일치하는"
+            fullNameLabel.text = "결과가 없어요"
+        }
+        
+    }
+    
+}
--- SlangQuiz/SlangQuiz/Source/Model/MyDB.swift
+++ SlangQuiz/SlangQuiz/Source/Model/MyDB.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 struct MyDB {
-    static var slangList: [Slang] = []
+    static var slangList: [Slang] = [
+        Slang(shortName: "갑분싸", fullName: "갑자기 분위기 싸해지다."),
+        Slang(shortName: "어쩔티비", fullName: "어쩌라고. 티비나 봐."),
+        Slang(shortName: "오점메추", fullName: "오늘 점심 메뉴 추천"),
+        Slang(shortName: "낄끼빠빠", fullName: "낄때 끼고, 빠질땐 빠져라"),
+        Slang(shortName: "인싸", fullName: "인사이더(insider)의 줄임말."),
+        Slang(shortName: "왹져", fullName: "외계인을 소재로 하거나, 외계인 자체를 이르는 말"),
+        Slang(shortName: "법브레스유", fullName: "법이 너를 지켜준다는 말로, 법이 아니었으면 너는 죽*다(ㅋㅋ)는 뜻"),
+        Slang(shortName: "킹받다", fullName: "엄청 화나다"),
+        Slang(shortName: "돈미새", fullName: "돈에 *친 *끼"),
+        Slang(shortName: "커엽다", fullName: "귀엽다")
+    ]
 }

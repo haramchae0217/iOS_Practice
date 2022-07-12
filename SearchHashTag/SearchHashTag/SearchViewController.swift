4fa7751b3109a293d4becd0994a3d061cc1569b6
Chae Haram <djvemfla2@gmail.com>
2022-07-12 23:46:15 +0900

    검색결과 출력

--- SearchHashTag/SearchHashTag/SearchViewController.swift
+++ SearchHashTag/SearchHashTag/SearchViewController.swift
@@ -24,10 +24,24 @@ class SearchViewController: UIViewController {
 
 extension SearchViewController: UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        if let text = searchBar.text, hashTagList.contains(text) {
-            hashTagListLabel.text = text
+        if let text = searchBar.text {
+            var matches: [String] = []
+            matches = hashTagList.filter { $0.contains(text) }
+            
+            var hashtags: String = ""
+            for i in 0..<matches.count {
+                if i == matches.count - 1 {
+                    hashtags.append("#\(matches[i])")
+                    break
+                }
+                hashtags.append("#\(matches[i]), ")
+            }
+            
+            hashTagListLabel.text = hashtags
         } else {
             hashTagListLabel.text = "'\(searchBar.text!)'라는 해시태그는 존재하지 않습니다."
         }
     }
 }
+
+

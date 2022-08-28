ddf6218a9523ec52c56ef95fadaa852b49fa7983
Chae Haram <djvemfla2@gmail.com>
2022-08-28 23:21:44 +0900

    렘 투두 추가 연습

---
+++ RealmExample/RealmExample/AddToDoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  AddToDoViewController.swift
+//  RealmExample
+//
+//  Created by Chae_Haram on 2022/08/28.
+//
+
+import UIKit
+import RealmSwift
+
+class AddToDoViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var memoTextField: UITextField!
+    @IBOutlet weak var startDatePicker: UIDatePicker!
+    @IBOutlet weak var endDatePicker: UIDatePicker!
+    
+    let localRealm = try! Realm()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureRightBarButtonItem()
+    }
+    
+    func configureRightBarButtonItem() {
+        let rightBarButtonItem = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(doneButtonClicked))
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    @objc func doneButtonClicked() {
+        let title = titleTextField.text!
+        let memo = memoTextField.text!
+        let startDate = startDatePicker.date
+        let endDate = endDatePicker.date
+        let todo = ToDo(title: title, memo: memo, startDate: startDate, endDate: endDate)
+        
+        try! localRealm.write {
+            localRealm.add(todo)
+        }
+        
+    }
+
+}

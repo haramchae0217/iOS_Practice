1d7ead2ed96f67ea7d07d4555d23404949db5ad9
Chae Haram <djvemfla2@gmail.com>
2022-08-31 20:51:38 +0900

    쇼핑 리스트 셀 작성

---
+++ RealmExample/RealmExample/Realm_array_Ex/ShoppingTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ShoppingTableViewCell.swift
+//  RealmExample
+//
+//  Created by Chae_Haram on 2022/08/31.
+//
+
+import UIKit
+
+class ShoppingTableViewCell: UITableViewCell {
+
+    static let identifier = "ShoppingCell"
+    @IBOutlet weak var itemNameLabel: UILabel!
+    
+}

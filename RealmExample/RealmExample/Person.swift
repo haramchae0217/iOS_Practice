44da818d47e1dd86947383a3a114548685c1bb19
Chae Haram <djvemfla2@gmail.com>
2022-08-31 20:52:04 +0900

    기본 구문 수정

--- RealmExample/RealmExample/Person.swift
+++ RealmExample/RealmExample/Person.swift
@@ -1,21 +1,21 @@
+////
+////  Person.swift
+////  RealmExample
+////
+////  Created by Chae_Haram on 2022/08/28.
+////
 //
-//  Person.swift
-//  RealmExample
+//import Foundation
+//import RealmSwift
 //
-//  Created by Chae_Haram on 2022/08/28.
+//class Person: Object {
+//    @Persisted(primaryKey: true) var _id: ObjectId
+//    @Persisted var name: String
+//    @Persisted var age: Int
 //
-
-import Foundation
-import RealmSwift
-
-class Person: Object {
-    @Persisted(primaryKey: true) var _id: ObjectId
-    @Persisted var name: String
-    @Persisted var age: Int
-
-    convenience init(name: String, age: Int) {
-        self.init()
-        self.name = name
-        self.age = age
-    }
-}
+//    convenience init(name: String, age: Int) {
+//        self.init()
+//        self.name = name
+//        self.age = age
+//    }
+//}
